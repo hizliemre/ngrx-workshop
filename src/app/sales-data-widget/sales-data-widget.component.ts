c32d75b24c56b6fae7389839480b462237da30ae
Emre Hızlı <hizliemre@gmail.com>
2022-05-01 15:42:11 +0300

    step5

--- src/app/sales-data-widget/sales-data-widget.component.ts
+++ src/app/sales-data-widget/sales-data-widget.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { take } from 'rxjs';
 import { SalesDataService } from '../api/sales-data.service';
 
 @Component({
@@ -14,8 +15,18 @@ export class SalesDataWidgetComponent implements OnInit {
   constructor(private readonly _salesDataService: SalesDataService) { }
 
   ngOnInit(): void {
+    this._getData();
+  }
+
+  refresh(): void {
+    this._getData();
+  }
+
+  private _getData(): void {
+    this.data = null;
     this._salesDataService
       .getSalesData(this.category)
+      .pipe(take(1))
       .subscribe((data) => this.data = data);
   }
 
