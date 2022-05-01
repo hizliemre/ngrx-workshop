a04ad347a7b496883344a52838b5565312c95a0c
Emre Hızlı <hizliemre@gmail.com>
2022-05-01 15:30:07 +0300

    step5

--- src/app/dashboard-1/dashboard-1.component.ts
+++ src/app/dashboard-1/dashboard-1.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SalesDataService } from '../api/sales-data.service';
 
 @Component({
   selector: 'dashboard-1',
@@ -7,10 +6,7 @@ import { SalesDataService } from '../api/sales-data.service';
 })
 export class Dashboard1Component implements OnInit {
 
-  electronicSalesData$ = this._salesDataService.getSalesData('electronic');
-  bookSalesData$ = this._salesDataService.getSalesData('book');
-
-  constructor(private readonly _salesDataService: SalesDataService) { }
+  constructor() { }
 
   ngOnInit(): void { }
 
--- src/app/sales-data-widget/sales-data-widget.component.ts
+++ src/app/sales-data-widget/sales-data-widget.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SalesDataService } from '../api/sales-data.service';
 
 @Component({
   selector: 'sales-data-widget',
   templateUrl: './sales-data-widget.component.html',
 })
-export class SalesDataWidgetComponent {
+export class SalesDataWidgetComponent implements OnInit {
 
-  @Input() data: any;
+  @Input() category: string = '';
+
+  data: any;
+
+  constructor(private readonly _salesDataService: SalesDataService) { }
+
+  ngOnInit(): void {
+    this._salesDataService
+      .getSalesData(this.category)
+      .subscribe((data) => this.data = data);
+  }
 
 }
