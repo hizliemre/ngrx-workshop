26f0703fa1894e64db181f6002458130eddbe630
Emre Hızlı <hizliemre@gmail.com>
2022-05-01 19:04:48 +0300

    step7

--- src/app/sales-data-widget/sales-data-widget.component.ts
+++ src/app/sales-data-widget/sales-data-widget.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { take } from 'rxjs';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subject, switchMap, takeUntil, tap } from 'rxjs';
 import { SalesDataService } from '../api/sales-data.service';
 
 @Component({
   selector: 'sales-data-widget',
   templateUrl: './sales-data-widget.component.html',
 })
-export class SalesDataWidgetComponent implements OnInit {
+export class SalesDataWidgetComponent implements OnInit, OnDestroy {
 
   @Input() category: string = '';
 
@@ -14,27 +14,41 @@ export class SalesDataWidgetComponent implements OnInit {
   loading: boolean;
   loaded: boolean;
 
+  private _destroy$ = new Subject<void>();
+  private _apiTrigger$ = new Subject<void>();
+
   constructor(private readonly _salesDataService: SalesDataService) { }
 
   ngOnInit(): void {
-    this._getData();
+    this.initAsyncs();
+    this._apiTrigger$.next();
   }
 
   refresh(): void {
-    this._getData();
+    this._apiTrigger$.next();
   }
 
-  private _getData(): void {
-    this.loading = true;
-    this.loaded = false;
-    this._salesDataService
-      .getSalesData(this.category)
-      .pipe(take(1))
-      .subscribe((data) => {
-        this.data = data;
-        this.loading = false;
-        this.loaded = true;
-      });
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 
+  private initAsyncs(): void {
+    this._apiTrigger$.pipe(
+      takeUntil(this._destroy$),
+      switchMap(() => {
+        this.loading = true;
+        this.loaded = false;
+        return this._salesDataService.getSalesData(this.category)
+          .pipe(
+            takeUntil(this._destroy$),
+            tap((data) => {
+              this.data = data;
+              this.loading = false;
+              this.loaded = true;
+            })
+          );
+      }),
+    ).subscribe();
+  }
 }
