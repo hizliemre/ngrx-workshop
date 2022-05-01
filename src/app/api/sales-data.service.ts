993d92474567f10240fcc622ce12ed22b340cb52
Emre Hızlı <hizliemre@gmail.com>
2022-05-01 15:52:05 +0300

    step1 fix

--- src/app/api/sales-data.service.ts
+++ src/app/api/sales-data.service.ts
@@ -10,7 +10,7 @@ export class SalesDataService {
   constructor(private readonly _http: HttpClient) { }
 
   getSalesData(category: string): Observable<any> {
-    return this._http.get(`api/sales-data/${category}`)
+    return this._http.get(`http://localhost:3000/sales-data/${category}`)
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Dashboard1Component } from './dashboard-1/dashboard-1.component';
 import { Dashboard2Component } from './dashboard-2/dashboard-2.component';
-import { Database } from './database';
-
 
 @NgModule({
   declarations: [
@@ -18,10 +15,7 @@ import { Database } from './database';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(Database, {
-      delay: 2000
-    }),
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
