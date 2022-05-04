17f57c65cc26620e26a35aac4be509bde3368920
Emre Hızlı <hizliemre@gmail.com>
2022-05-04 12:24:07 +0300

    step11 fix

--- src/app/sales-data-widget/+state/actions.ts
+++ src/app/sales-data-widget/+state/actions.ts
@@ -1,15 +1,5 @@
-import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { createActionGroup, props } from '@ngrx/store';
 import { SalesDataWidgetData } from 'src/app/widget-data.model';
-
-
-export const stateActions = createActionGroup({
-  source: 'Sales Data Widget/STATE',
-  events: {
-    'Initialize': props<{ category: string }>(),
-    'Initialized': emptyProps(),
-    'Destroy': emptyProps(),
-  }
-})
 
 export const getDataActions = createActionGroup({
   source: 'Sales Data Widget/API',
