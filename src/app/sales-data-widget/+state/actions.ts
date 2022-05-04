671e07019e1615d21760c14a23b99a2765c4bcaa
Emre Hızlı <hizliemre@gmail.com>
2022-05-04 14:54:40 +0300

    10 fix

--- src/app/sales-data-widget/+state/actions.ts
+++ src/app/sales-data-widget/+state/actions.ts
@@ -2,5 +2,5 @@ import { createAction, props } from '@ngrx/store';
 import { SalesDataWidgetData } from 'src/app/widget-data.model';
 
 export const getData = createAction('[Sales Data Widget/API] Get Data', props<{ category: string }>());
-export const getDataSuccess = createAction('[Sales Data Widget/API] Get Data Success', props<{ data: SalesDataWidgetData[] }>());
+export const getDataSuccess = createAction('[Sales Data Widget/API] Get Data Success', props<{ data: SalesDataWidgetData }>());
 export const getDataFail = createAction('[Sales Data Widget/API] Get Data Fail', props<{ error: Error }>());
