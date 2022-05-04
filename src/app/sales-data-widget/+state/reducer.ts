e19a290d22ca49dac6c8269c8ada5b2a5146c7ab
Emre Hızlı <hizliemre@gmail.com>
2022-05-04 14:44:22 +0300

    12 fix

--- src/app/sales-data-widget/+state/reducer.ts
+++ src/app/sales-data-widget/+state/reducer.ts
@@ -3,5 +3,5 @@ import { SalesDataWidgetData } from 'src/app/widget-data.model';
 export interface SalesDataWidgetState {
   loading: boolean;
   loaded: boolean;
-  data: SalesDataWidgetData[]
+  data: SalesDataWidgetData;
 };
