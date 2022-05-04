80f22408c3bc7483e5d353eece9e7598990d7b77
Emre Hızlı <hizliemre@gmail.com>
2022-05-04 14:21:49 +0300

    step13

--- src/app/sales-data-widget/+state/reducer.ts
+++ src/app/sales-data-widget/+state/reducer.ts
@@ -1,7 +1,14 @@
 import { SalesDataWidgetData } from 'src/app/widget-data.model';
 
+export const SALES_DATA_WIDGET_FEATURE_KEY = 'salesDataWidget';
 export interface SalesDataWidgetState {
   loading: boolean;
   loaded: boolean;
   data: SalesDataWidgetData[]
+};
+
+const initialState: SalesDataWidgetState = {
+  loading: false,
+  loaded: false,
+  data: []
 };
