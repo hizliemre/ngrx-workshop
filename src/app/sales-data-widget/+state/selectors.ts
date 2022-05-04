af96a398ad79e46748c51480772b9625c60aef76
Emre Hızlı <hizliemre@gmail.com>
2022-05-04 15:21:53 +0300

    step21

--- src/app/sales-data-widget/+state/selectors.ts
+++ src/app/sales-data-widget/+state/selectors.ts
@@ -5,7 +5,21 @@ import { salesDataWidgetFeature } from './reducer';
 const selectViewModel = createSelector(
   salesDataWidgetFeature.selectLoading,
   salesDataWidgetFeature.selectLoaded,
-  salesDataWidgetFeature.selectData, (loading, loaded, data) => ({ loading, loaded, data: data as SalesDataWidgetData, })
+  salesDataWidgetFeature.selectData, (loading, loaded, data) => ({
+    loading,
+    loaded,
+    data: data as SalesDataWidgetData,
+  })
+);
+
+// direkt state'in tamamını da select edebiliriz.
+const selectViewModel2 = createSelector(
+  salesDataWidgetFeature.selectSalesDataWidgetState,
+  (state) => ({
+    loading: state.loading,
+    loaded: state.loaded,
+    data: state.data as SalesDataWidgetData,
+  })
 );
 
 export const salesDataWidgetSelectors = {
