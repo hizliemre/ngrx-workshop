d7f42b55ffaa3b2afd2f73b5b029353c84237637
Emre Hızlı <hizliemre@gmail.com>
2022-05-05 17:58:14 +0300

    fix

--- src/app/sales-data-widget/+state/effects.ts
+++ src/app/sales-data-widget/+state/effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, filter, map, of, switchMap } from 'rxjs';
+import { Actions, createEffect } from '@ngrx/effects';
+import { catchError, map, of, switchMap } from 'rxjs';
 import { SalesDataService } from 'src/app/api/sales-data.service';
 import { IdentifiedEffects } from 'src/app/ngrx-infra';
 import { getDataActions } from './actions';
@@ -15,8 +15,7 @@ export class SalesDataWidgetEffects extends IdentifiedEffects {
 
   $getData = createEffect(() => {
     return this._actions$.pipe(
-      ofType(getDataActions.getData),
-      filter(({ identifier }) => this.identifier == identifier),
+      this.localOfType(getDataActions.getData),
       switchMap(({ category }) => this._salesDataService.getSalesData(category)
         .pipe(
           map((data) => getDataActions.getDataSuccess({ identifier: this.identifier, data })),
