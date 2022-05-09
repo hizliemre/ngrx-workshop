a9d3b7197b951a4756bbf8f80f86039bbace4c6f
Emre Hızlı <hizliemre@gmail.com>
2022-05-09 14:39:22 +0300

    28

--- src/app/ngrx-infra.ts
+++ src/app/ngrx-infra.ts
@@ -1,9 +1,17 @@
-import { ActionCreatorProps } from '@ngrx/store';
-import { NotAllowedInPropsCheck } from '@ngrx/store/src/models';
+import { ActionCreatorProps, UPDATE } from '@ngrx/store';
+import { Action, ActionReducer, NotAllowedInPropsCheck } from '@ngrx/store/src/models';
 
 export function emptyIdentifedProps(): ActionCreatorProps<{ identifier: string }> {
   return identifedProps();
 }
 export function identifedProps<P extends SafeProps, SafeProps = NotAllowedInPropsCheck<P>>(): ActionCreatorProps<P & { identifier: string }> {
   return { _as: 'props', _p: undefined! };
+}
+
+type IdentifiedAction = Action & { identifier: string };
+
+export const filterReducer = <T>(identifier: string, reducer: ActionReducer<T>) => (state: T, action: IdentifiedAction) => {
+  if (action.type === UPDATE) return reducer(state, action);
+  if (action.identifier && action.identifier === identifier) return reducer(state, action);
+  return state;
 }
--- src/app/sales-data-widget/sales-data-widget.component.ts
+++ src/app/sales-data-widget/sales-data-widget.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { EffectSources } from '@ngrx/effects';
-import { ActionReducer, ReducerManager, Store, UPDATE } from '@ngrx/store';
+import { ReducerManager, Store } from '@ngrx/store';
 import { Feature } from '@ngrx/store/src/feature_creator';
 import { Guid } from 'guid-typescript';
 import { Observable } from 'rxjs';
+import { filterReducer } from '../ngrx-infra';
 import { getDataActions } from './+state/actions';
 import { SalesDataWidgetEffects } from './+state/effects';
 import { salesDataWidgetFeature, SalesDataWidgetState } from './+state/reducer';
@@ -33,7 +34,7 @@ export class SalesDataWidgetComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this._feature = salesDataWidgetFeature(this._identifier);
-    this._reducerManager.addReducer(this._feature.name, this.filterReducer(this._identifier, this._feature.reducer));
+    this._reducerManager.addReducer(this._feature.name, filterReducer(this._identifier, this._feature.reducer));
     this._effects.init(this._identifier);
     this._effectSources.addEffects(this._effects);
     this.initAsyncs();
@@ -46,12 +47,6 @@ export class SalesDataWidgetComponent implements OnInit, OnDestroy {
 
   refresh(): void {
     this._store.dispatch(getDataActions.getData({ identifier: this._identifier, category: this.category }));
-  }
-
-  filterReducer = <T>(identifier: string, reducer: ActionReducer<T>) => (state: T, action: any) => {
-    if (action.type === UPDATE) return reducer(state, action);
-    if (action.identifier && action.identifier === identifier) return reducer(state, action);
-    return state;
   }
 
   private initAsyncs(): void {
